9a2e2de095dc22347f0274f75c44cabd6f51119a
Alejandro <alejandrojuarez675@gmail.com>
2019-01-26 23:42:29 -0300

    funcionamiento basico

--- listaDeCompras/src/app/app.module.ts
+++ listaDeCompras/src/app/app.module.ts
@@ -1,23 +1,37 @@
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatButtonToggleModule, MatDialogModule } from "@angular/material";
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { Dialogs } from '@ionic-native/dialogs/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  exports: [
+    CdkTableModule,
+    CdkTreeModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatButtonToggleModule,
+    BrowserAnimationsModule
+  ],
+  imports: [BrowserModule,     
+    BrowserModule,
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    IonicStorageModule.forRoot()
+  ],
   providers: [
-    StatusBar,
-    SplashScreen,
+    Dialogs,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- listaDeCompras/src/app/home/home.page.ts
+++ listaDeCompras/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Injectable } from '@angular/core';
+import { Dialogs } from '@ionic-native/dialogs/ngx';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +10,56 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  lista: Item[] = new Array;
+
+  constructor(
+    public dialog: MatDialog, 
+    private dialogs: Dialogs,
+    private storage: Storage
+    ) {
+      this.storage.keys().then( keys =>{
+        keys.forEach( k => {
+          this.storage.get(k).then( item => {
+            this.lista.push(item);
+          });
+        });
+      }).catch( x => {
+        this.lista = new Array;
+      });
+  }
+
+  onAdd() {
+    this.dialogs.prompt("Que desea agregar?", "Add", [], "").then(dat => {
+      let itemAux = new Item((this.lista.length + 1), dat.input1);
+      this.lista.push(itemAux);
+      this.storage.set(itemAux.codigo.toString(), itemAux);
+    }).catch(e => console.log('Error displaying dialog', e));
+
+  }
+
+  onDelete(codigo: number) {
+    let listaAux: Item[] = new Array;
+    let cont = 1;
+    this.storage.clear();
+    this.lista.forEach(element => {
+      if (element.codigo != codigo) {
+        element.codigo = cont;
+        listaAux.push(element);
+        this.storage.set(cont.toString(), element);
+        cont++;
+      }
+    });
+    this.lista = listaAux;
+  }
+
+}
+
+export class Item {
+  codigo: number;
+  descripcion: string;
+
+  constructor(codigo, descripcion) {
+    this.codigo = codigo;
+    this.descripcion = descripcion;
+  }
 }
